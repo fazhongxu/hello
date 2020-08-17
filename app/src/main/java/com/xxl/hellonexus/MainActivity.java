d22b9aac687a113c17680cda1a4d5101fa5840fb
fazhongxu <xuzf@edujia.com>
2020-08-17 11:53:29 +0800

    first commit

---
+++ app/src/main/java/com/xxl/hellonexus/MainActivity.java
@@ -0,0 +1,21 @@
+package com.xxl.hellonexus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.xxl.common.TestUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int random = TestUtils.getRandom();
+        TextView tvText = findViewById(R.id.tv_test);
+        tvText.setText(String.valueOf(random));
+    }
+}
