08eb093c517a943aeb7a6f419db3351665be85ed
fazhongxu <xuzf@edujia.com>
2020-08-20 20:42:53 +0800

    优化flutter引擎加载的位置

--- app/src/main/java/com/xxl/hellonexus/MainActivity.java
+++ app/src/main/java/com/xxl/hellonexus/MainActivity.java
@@ -8,22 +8,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.xxl.common.TestUtils;
 
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.embedding.engine.FlutterEngineCache;
-import io.flutter.embedding.engine.dart.DartExecutor;
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    public static String FLUTTER_ENGINE = "flutter_engine";
-
-    private FlutterEngine mFlutterEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
         int random = TestUtils.getRandom();
         TextView tvText = findViewById(R.id.tv_test);
         tvText.setText(String.valueOf(random));
@@ -35,25 +28,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * 初始化Flutter引擎
-     */
-    private void init() {
-        mFlutterEngine = new FlutterEngine(this);
-        mFlutterEngine.getDartExecutor().executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault());
-        FlutterEngineCache.getInstance().put(FLUTTER_ENGINE, mFlutterEngine);
-    }
-
-    /**
      * 跳转Flutter页面
      */
     private void startFlutterActivity() {
-        startActivity(FlutterActivity.withCachedEngine(FLUTTER_ENGINE)
-                .build(this));
+        startActivity(FlutterActivity.withCachedEngine(MyApplication.FLUTTER_ENGINE_ID)
+                .build(this)
+        );
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mFlutterEngine.destroy();
-    }
 }
---
+++ app/src/main/java/com/xxl/hellonexus/MyApplication.java
@@ -0,0 +1,33 @@
+package com.xxl.hellonexus;
+
+import android.app.Application;
+
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.FlutterEngineCache;
+import io.flutter.embedding.engine.dart.DartExecutor;
+
+/**
+ * @author xxl.
+ * @date 2020/8/20.
+ */
+public class MyApplication extends Application {
+
+    public static final String FLUTTER_ENGINE_ID = "flutter_engine_id";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Instantiate a FlutterEngine.
+        FlutterEngine flutterEngine = new FlutterEngine(this);
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.getDartExecutor().executeDartEntrypoint(
+                DartExecutor.DartEntrypoint.createDefault()
+        );
+
+        // Cache the FlutterEngine to be used by FlutterActivity.
+        FlutterEngineCache
+                .getInstance()
+                .put(FLUTTER_ENGINE_ID, flutterEngine);
+    }
+}
