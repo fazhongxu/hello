579e07032cbcfd4bbcf7038316f20b765e7a7232
fazhongxu <xuzf@edujia.com>
2021-07-16 16:38:52 +0800

    优化登录页面代码

--- module_user/src/main/java/com/xxl/user/ui/LoginActivity.java
+++ module_user/src/main/java/com/xxl/user/ui/LoginActivity.java
@@ -2,14 +2,10 @@ package com.xxl.user.ui;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.xxl.service.ui.BaseViewModel;
-import com.xxl.service.ui.ViewModelProviderFactory;
 import com.xxl.user.R;
 
 import javax.inject.Inject;
@@ -38,12 +34,13 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        mBtnLogin = findViewById(R.id.btn_login);
         if (enableInjection()) {
             AndroidInjection.inject(this);
         }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mBtnLogin = findViewById(R.id.btn_login);
+
         mLoginActivityViewModel = createViewModel();
         mBtnLogin.setOnClickListener(v -> {
             mLoginActivityViewModel.requestLogin("123456", "abc");
