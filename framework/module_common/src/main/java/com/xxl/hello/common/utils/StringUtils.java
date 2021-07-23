b1f772fa00b522b8cd20aad153920b39aa3a82f7
fazhongxu <xuzf@edujia.com>
2021-07-23 20:23:53 +0800

    add FileUtils

---
+++ framework/module_common/src/main/java/com/xxl/hello/common/utils/StringUtils.java
@@ -0,0 +1,38 @@
+package com.xxl.hello.common.utils;
+
+/**
+ * @author xxl.
+ * @date 2021/7/23.
+ */
+public class StringUtils {
+
+    //region: 成员变量
+
+    //endregion
+
+    //region: 构造函数
+
+    private StringUtils() {
+
+    }
+
+    public final static StringUtils obtain() {
+        return new StringUtils();
+    }
+
+    public static boolean isSpace(String filePath) {
+        // TODO: 2021/7/23  
+        return false;
+    }
+
+    //endregion
+
+    //region: 提供方法
+
+    //endregion
+
+    //region: 内部辅助方法
+
+    //endregion
+
+}
