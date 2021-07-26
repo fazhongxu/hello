f2b0c337f0534ee86cc359113d642ad4d1524b22
fazhongxu <xuzf@edujia.com>
2021-07-26 10:46:28 +0800

    add PathUtils,FileUtils

--- framework/module_common/src/main/java/com/xxl/hello/common/utils/StringUtils.java
+++ framework/module_common/src/main/java/com/xxl/hello/common/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package com.xxl.hello.common.utils;
 
+import androidx.annotation.Nullable;
+
 /**
  * @author xxl.
  * @date 2021/7/23.
@@ -23,6 +25,35 @@ public class StringUtils {
     public static boolean isSpace(String filePath) {
         // TODO: 2021/7/23  
         return false;
+    }
+
+    /**
+     * 判断文本是否为空
+     *
+     * @param charSequence
+     * @return
+     */
+    public static boolean isEmpty(@Nullable final CharSequence charSequence) {
+        return charSequence == null || charSequence.length() <= 0;
+    }
+
+    /**
+     * 将文本转换为long 类型
+     *
+     * @param text
+     * @param defaultValue
+     * @return
+     */
+    public static long toLong(@Nullable final String text,
+                              final int defaultValue) {
+        if (!isEmpty(text)) {
+            try {
+                return Long.parseLong(text);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return defaultValue;
     }
 
     //endregion
