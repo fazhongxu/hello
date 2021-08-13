cfc3d2988fa7d58dfec6c845b49ad182dc356116
fazhongxu <xuzf@edujia.com>
2021-08-13 23:14:11 +0800

    splash页面UI细节调整

--- framework/module_core/src/androidTest/java/com/xxl/common/ExampleInstrumentedTest.java
+++ framework/module_core/src/androidTest/java/com/xxl/common/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.xxl.module_common.test", appContext.getPackageName());
+        assertEquals("com.xxl.hello.common.test", appContext.getPackageName());
     }
 }
--- framework/module_widget/src/androidTest/java/com/xxl/resources/ExampleInstrumentedTest.java
+++ framework/module_widget/src/androidTest/java/com/xxl/resources/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.xxl.module_resources", appContext.getPackageName());
+        assertEquals("com.xxl.hello.widget", appContext.getPackageName());
     }
 }
