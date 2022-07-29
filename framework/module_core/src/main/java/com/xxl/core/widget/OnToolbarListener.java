dbe50222f236198fbb551a46ff7576b3a81f584d
fazhongxu <xuzf@edujia.com>
2022-07-30 01:00:21 +0800

    添加监听

---
+++ framework/module_core/src/main/java/com/xxl/core/widget/OnToolbarListener.java
@@ -0,0 +1,17 @@
+package com.xxl.core.widget;
+
+import androidx.annotation.NonNull;
+
+/**
+ * @author xxl.
+ * @date 2022/7/30.
+ */
+public interface OnToolbarListener {
+
+    /**
+     * 设置标题
+     *
+     * @param title
+     */
+    void setToolbarTitle(@NonNull CharSequence title);
+}
