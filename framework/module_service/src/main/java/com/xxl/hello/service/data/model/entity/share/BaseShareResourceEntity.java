0191d5aaf46021b11f726c4ea9de389571b933b3
fazhongxu <xuzf@edujia.com>
2022-08-01 18:29:18 +0800

    分享优化

--- framework/module_service/src/main/java/com/xxl/hello/service/data/model/entity/share/BaseShareResourceEntity.java
+++ framework/module_service/src/main/java/com/xxl/hello/service/data/model/entity/share/BaseShareResourceEntity.java
@@ -1,13 +1,6 @@
 package com.xxl.hello.service.data.model.entity.share;
 
-import android.text.TextUtils;
-
 import androidx.annotation.NonNull;
-
-import com.xxl.kit.ListUtils;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 资源分享实体基础类
@@ -20,9 +13,14 @@ public abstract class BaseShareResourceEntity<T extends BaseShareResourceEntity>
     //region: 成员变量
 
     /**
-     * 多媒体数据
+     * item垂直间隙
      */
-    private List<ShareMediaEntity> mShareMediaEntities;
+    private String mVerticalSpancing;
+
+    /**
+     * item水平间隙
+     */
+    private String mHorizontalSpancing;
 
     /**
      * 描述文本
@@ -43,32 +41,6 @@ public abstract class BaseShareResourceEntity<T extends BaseShareResourceEntity>
     //region: 提供方法
 
     /**
-     * 获取多媒体地址
-     *
-     * @return
-     */
-    public List<String> getShareMediaUrls() {
-        final List<String> mediaUrls = new ArrayList<>();
-        if (!ListUtils.isEmpty(mShareMediaEntities)) {
-            for (ShareMediaEntity shareMediaEntity : mShareMediaEntities) {
-                if (TextUtils.isEmpty(shareMediaEntity.getMediaUrl())) {
-                    mediaUrls.add(shareMediaEntity.getMediaUrl());
-                }
-            }
-        }
-        return mediaUrls;
-    }
-
-    /**
-     * 获取多媒体数据
-     *
-     * @return
-     */
-    public List<ShareMediaEntity> getShareMediaEntities() {
-        return mShareMediaEntities;
-    }
-
-    /**
      * 获取描述文本
      *
      * @return
@@ -76,18 +48,7 @@ public abstract class BaseShareResourceEntity<T extends BaseShareResourceEntity>
     public String getDescription() {
         return mDescription;
     }
-
-    /**
-     * 设置多媒体数据
-     *
-     * @param mediaEntities
-     * @return
-     */
-    public T setShareMediaEntities(@NonNull final List<ShareMediaEntity> mediaEntities) {
-        mShareMediaEntities = mediaEntities;
-        return (T) this;
-    }
-
+    
     /**
      * 设置描述文本
      *
--- framework/module_service/src/main/java/com/xxl/hello/service/data/model/entity/share/ImageShareResourceEntity.java
+++ framework/module_service/src/main/java/com/xxl/hello/service/data/model/entity/share/ImageShareResourceEntity.java
@@ -1,5 +1,14 @@
 package com.xxl.hello.service.data.model.entity.share;
 
+import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+
+import com.xxl.kit.ListUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 图片分享数据
  *
@@ -9,6 +18,11 @@ package com.xxl.hello.service.data.model.entity.share;
 public class ImageShareResourceEntity extends BaseShareResourceEntity<ImageShareResourceEntity> {
 
     //region: 成员变量
+
+    /**
+     * 多媒体数据
+     */
+    private List<ShareMediaEntity> mShareMediaEntities;
 
     //endregion
 
@@ -25,6 +39,43 @@ public class ImageShareResourceEntity extends BaseShareResourceEntity<ImageShare
     //endregion
 
     //region: 提供方法
+
+    /**
+     * 获取多媒体地址
+     *
+     * @return
+     */
+    public List<String> getShareMediaUrls() {
+        final List<String> mediaUrls = new ArrayList<>();
+        if (!ListUtils.isEmpty(mShareMediaEntities)) {
+            for (ShareMediaEntity shareMediaEntity : mShareMediaEntities) {
+                if (TextUtils.isEmpty(shareMediaEntity.getMediaUrl())) {
+                    mediaUrls.add(shareMediaEntity.getMediaUrl());
+                }
+            }
+        }
+        return mediaUrls;
+    }
+
+    /**
+     * 获取多媒体数据
+     *
+     * @return
+     */
+    public List<ShareMediaEntity> getShareMediaEntities() {
+        return mShareMediaEntities;
+    }
+
+    /**
+     * 设置多媒体数据
+     *
+     * @param mediaEntities
+     * @return
+     */
+    public ImageShareResourceEntity setShareMediaEntities(@NonNull final List<ShareMediaEntity> mediaEntities) {
+        mShareMediaEntities = mediaEntities;
+        return this;
+    }
 
     //endregion
 
