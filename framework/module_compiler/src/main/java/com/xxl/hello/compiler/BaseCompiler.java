a9e631d4e734fb201feb7683176e7c22cb466cf5
fazhongxu <xuzf@edujia.com>
2022-08-17 10:03:12 +0800

    注解处理器添加参考文档注释

--- framework/module_compiler/src/main/java/com/xxl/hello/compiler/BaseCompiler.java
+++ framework/module_compiler/src/main/java/com/xxl/hello/compiler/BaseCompiler.java
@@ -5,12 +5,18 @@ import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
+import javax.tools.Diagnostic;
 
 /**
  * 注解处理器
- * reference https://www.jianshu.com/p/07ef8ba80562
+ * <p>
+ * reference
+ * https://www.jianshu.com/p/07ef8ba80562
+ * https://www.jianshu.com/p/18b20f82db78?utm_campaign=haruki&utm_content=note&utm_medium=reader_share&utm_source=weixin
+ * https://blog.csdn.net/qq_45295475/article/details/121595963
  *
  * @author xxl.
  * @date 2022/8/16.
@@ -38,4 +44,24 @@ public abstract class BaseCompiler extends AbstractProcessor {
     public SourceVersion getSupportedSourceVersion() {
         return SourceVersion.latestSupported();
     }
+
+    public void error(Element element, String message, Object... args) {
+        printMessage(Diagnostic.Kind.ERROR, element, message, args);
+    }
+
+    public void waring(Element element, String message, Object... args) {
+        printMessage(Diagnostic.Kind.WARNING, element, message, args);
+    }
+
+    public void note(Element element, String message, Object... args) {
+        printMessage(Diagnostic.Kind.NOTE, element, message, args);
+    }
+
+    public void printMessage(Diagnostic.Kind kind, Element element, String message, Object... args) {
+        if (args.length > 0) {
+            message = String.format(message, args);
+        }
+        mMessager.printMessage(kind, message, element);
+    }
+
 }
