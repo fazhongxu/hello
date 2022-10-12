0836320b29dfc76230b1f45334ae128c4968b4f6
fazhongxu <xuzf@edujia.com>
2022-10-12 14:20:01 +0800

    完善recyclerview条目间距设置

--- framework/module_core/src/main/java/com/xxl/core/utils/DecorationUtils.java
+++ framework/module_core/src/main/java/com/xxl/core/utils/DecorationUtils.java
@@ -1,6 +1,7 @@
 package com.xxl.core.utils;
 
 import com.xxl.core.widget.recyclerview.decoration.HorizontalDividerItemDecoration;
+import com.xxl.core.widget.recyclerview.decoration.VerticalDividerItemDecoration;
 import com.xxl.kit.AppUtils;
 import com.xxl.kit.DisplayUtils;
 
@@ -38,6 +39,23 @@ public class DecorationUtils {
                 .color(color)
                 .size(dividerSize)
                 .margin(DisplayUtils.dp2px(AppUtils.getApplication(), marginLeft), DisplayUtils.dp2px(AppUtils.getApplication(), marginRight))
+                .build();
+    }
+
+    /**
+     * 创建水平方向的RecyclerView列表间距
+     *
+     * @param color        颜色
+     * @param dividerSize  下划线宽度
+     * @param marginTop    上边间距
+     * @param marginBottom 下边间距
+     * @return
+     */
+    public static VerticalDividerItemDecoration createVerticalDividerItemDecoration(int color, int dividerSize, int marginTop, int marginBottom) {
+        return new VerticalDividerItemDecoration.Builder(AppUtils.getApplication())
+                .color(color)
+                .size(dividerSize)
+                .margin(DisplayUtils.dp2px(AppUtils.getApplication(), marginTop), DisplayUtils.dp2px(AppUtils.getApplication(), marginBottom))
                 .build();
     }
 
