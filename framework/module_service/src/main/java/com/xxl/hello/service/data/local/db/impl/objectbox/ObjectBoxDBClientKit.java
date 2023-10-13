3ac6cf2d534f75985df39a2138650e0a42593597
fazhongxu <xuzf@edujia.com>
2023-10-13 10:49:12 +0800

    objectbox浏览数据库过时类处理

--- framework/module_service/src/main/java/com/xxl/hello/service/data/local/db/impl/objectbox/ObjectBoxDBClientKit.java
+++ framework/module_service/src/main/java/com/xxl/hello/service/data/local/db/impl/objectbox/ObjectBoxDBClientKit.java
@@ -13,7 +13,7 @@ import com.xxl.hello.service.data.local.prefs.api.UserPreferences;
 import com.xxl.kit.LogUtils;
 
 import io.objectbox.BoxStore;
-import io.objectbox.android.AndroidObjectBrowser;
+import io.objectbox.android.Admin;
 
 /**
  * 数据库服务
@@ -42,7 +42,7 @@ public class ObjectBoxDBClientKit implements DBClientKit {
     /**
      * ObjectBox 数据库
      */
-    private BoxStore mOoxStore;
+    private BoxStore mBoxStore;
 
     /**
      * 应用上下文
@@ -81,19 +81,19 @@ public class ObjectBoxDBClientKit implements DBClientKit {
             }
 
             final String dataBaseName = buildDataBaseName(targetUserId);
-            if (TextUtils.equals(dataBaseName,mDatabaseName) && mOoxStore != null && !mOoxStore.isClosed()) {
+            if (TextUtils.equals(dataBaseName, mDatabaseName) && mBoxStore != null && !mBoxStore.isClosed()) {
                 return true;
             }
             mDatabaseName = dataBaseName;
 
             closeDatabase();
-            mOoxStore = MyObjectBox.builder()
+            mBoxStore = MyObjectBox.builder()
                     .androidContext(mApplication)
                     .name(dataBaseName)
                     .build();
 
             if (BuildConfig.DEBUG) {
-                final boolean start = new AndroidObjectBrowser(mOoxStore)
+                boolean start = new Admin(mBoxStore)
                         .start(mApplication);
                 LogUtils.d("数据库网页浏览服务开启状态" + start);
             }
@@ -107,11 +107,11 @@ public class ObjectBoxDBClientKit implements DBClientKit {
     @Override
     public void closeDatabase() {
         synchronized (this) {
-            if (mOoxStore == null || mOoxStore.isClosed()) {
+            if (mBoxStore == null || mBoxStore.isClosed()) {
                 return;
             }
-            mOoxStore.close();
-            mOoxStore = null;
+            mBoxStore.close();
+            mBoxStore = null;
         }
     }
 
@@ -123,7 +123,7 @@ public class ObjectBoxDBClientKit implements DBClientKit {
     @Override
     public boolean isClosed() {
         synchronized (this) {
-            return mOoxStore != null && mOoxStore.isClosed();
+            return mBoxStore != null && mBoxStore.isClosed();
         }
     }
 
@@ -134,10 +134,10 @@ public class ObjectBoxDBClientKit implements DBClientKit {
      */
     public BoxStore getOoxStore() {
         synchronized (this) {
-            if (mOoxStore == null || mOoxStore.isClosed()) {
+            if (mBoxStore == null || mBoxStore.isClosed()) {
                 switchDatabase(mUserPreferences.getUserId());
             }
-            return mOoxStore;
+            return mBoxStore;
         }
     }
 
