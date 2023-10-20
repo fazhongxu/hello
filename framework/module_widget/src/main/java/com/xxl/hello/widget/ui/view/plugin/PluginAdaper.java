4db921d55e9992a31199c92b516b0899fb47d857
fazhongxu <xuzf@edujia.com>
2023-10-20 10:52:36 +0800

    常用功能插件代码完善

--- framework/module_widget/src/main/java/com/xxl/hello/widget/ui/view/plugin/PluginAdaper.java
+++ framework/module_widget/src/main/java/com/xxl/hello/widget/ui/view/plugin/PluginAdaper.java
@@ -1,5 +1,16 @@
 package com.xxl.hello.widget.ui.view.plugin;
 
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+
+import com.xxl.hello.widget.R;
+import com.xxl.hello.widget.databinding.WidgetLayoutCommonPluginBinding;
+import com.xxl.kit.DisplayUtils;
+
 /**
  * @author xxl.
  * @date 2023/9/15.
@@ -7,6 +18,13 @@ package com.xxl.hello.widget.ui.view.plugin;
 public class PluginAdaper {
 
     //region: 成员变量
+
+    private LayoutInflater mLayoutInflater;
+
+    /**
+     * 插件视图
+     */
+    private WidgetLayoutCommonPluginBinding mPluginBinding;
 
     //endregion
 
@@ -23,6 +41,29 @@ public class PluginAdaper {
     //endregion
 
     //region: 提供方法
+
+    /**
+     * 绑定到视图上
+     *
+     * @param viewGroup
+     */
+    public void bindView(@NonNull final ViewGroup viewGroup) {
+        setupLayout(viewGroup);
+    }
+
+    /**
+     * 设置视图
+     *
+     * @param viewGroup
+     */
+    private void setupLayout(@NonNull final ViewGroup viewGroup) {
+        mLayoutInflater = LayoutInflater.from(viewGroup.getContext());
+        mPluginBinding = DataBindingUtil.inflate(mLayoutInflater, R.layout.widget_layout_common_plugin, viewGroup, true);
+        // TODO: 2023/10/20 dimin 引用键盘高度
+        int height = DisplayUtils.dp2px(viewGroup.getContext(), 250);
+        mPluginBinding.getRoot().setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height));
+        // View Pager 设置adapter
+    }
 
     //endregion
 
