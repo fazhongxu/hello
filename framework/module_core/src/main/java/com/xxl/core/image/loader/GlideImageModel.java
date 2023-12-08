98a3d79f22bebc6a2dca61defc58af062e11ea23
xxl <xuzf@edujia.com>
2023-12-08 17:57:38 +0800

    Glide 加载图片使用OKHttp

--- framework/module_core/src/main/java/com/xxl/core/image/loader/GlideImageModel.java
+++ framework/module_core/src/main/java/com/xxl/core/image/loader/GlideImageModel.java
@@ -1,7 +1,22 @@
 package com.xxl.core.image.loader;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
+import com.xxl.core.utils.AppExpandUtils;
+import com.xxl.kit.LogUtils;
+
+import java.io.InputStream;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 /**
  * @author xxl.
@@ -10,14 +25,33 @@ import com.bumptech.glide.module.AppGlideModule;
 @GlideModule(glideName = "ImageLoader")
 public class GlideImageModel extends AppGlideModule {
 
-    //region: 成员变量
-
-    //endregion
-
     //region: 构造函数
 
     public GlideImageModel() {
 
+    }
+
+    //endregion
+
+    //region: 生命周期
+
+    @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        if (AppExpandUtils.isDebug()) {
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+                @Override
+                public void log(String message) {
+                    LogUtils.d("Glide OkHttp" + message);
+                }
+            });
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            builder.addInterceptor(loggingInterceptor);
+        }
+
+        OkHttpClient okHttpClient = builder.build();
+
+        registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(okHttpClient));
     }
 
     //endregion
