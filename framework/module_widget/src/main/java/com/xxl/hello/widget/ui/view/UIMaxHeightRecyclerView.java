a25c84560209b4f9bf328bace34d522e9c1bd88f
xxl <xuzf@edujia.com>
2023-12-25 17:02:03 +0800

    去除注释代码，需要看的时候看上次记录即可

--- framework/module_widget/src/main/java/com/xxl/hello/widget/ui/view/UIMaxHeightRecyclerView.java
+++ framework/module_widget/src/main/java/com/xxl/hello/widget/ui/view/UIMaxHeightRecyclerView.java
@@ -12,40 +12,6 @@ import com.xxl.kit.DisplayUtils;
 /**
  * 自定义最大高度的RecyclerView
  *
- *
- * <pre>
- * * 设置ScrollView视图
- *
- *     private void setupScrollView() {
- *         final View line = mSizeInfoSettingBinding.vLine;
- *         final NestedScrollView scrollView = mSizeInfoSettingBinding.svRootContainer;
- *         scrollView.setOnScrollChangeListener((NestedScrollView.OnScrollChangeListener) (v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
- *             if (isActivityFinishing()) {
- *                 return;
- *             }
- *             final boolean isVisibleLocal = isVisibleLocal(line);
- *             if (mSizeNameLayoutManager != null) {
- *                 mSizeNameLayoutManager.setScrollEnabled(!isVisibleLocal);
- *             }
- *             if (mSizeInfoLayoutManager != null) {
- *                 mSizeInfoLayoutManager.setScrollEnabled(!isVisibleLocal);
- *             }
- *         });
- *     }
- *
- *    /**
- *     * 判断View是否在屏幕上可见
- *     *
- *     * @param target
- *     * @return 当 View 有一点点不可见时立即返回false!
- *
- *   private boolean isVisibleLocal(@NonNull final View target) {
- *       Rect rect = new Rect();
- *       target.getLocalVisibleRect(rect);
- *       return rect.top == 0;
- *   }
- * </pre>
- *
  * @author xxl.
  * @date 2021/12/31.
  */
