3f6e110902a67adb21f591d5164264647859b0f6
xxl <xuzf@edujia.com>
2023-12-28 15:15:19 +0800

    画椭圆

--- framework/module_widget/src/main/java/com/xxl/hello/widget/ui/view/TestPathImageView.java
+++ framework/module_widget/src/main/java/com/xxl/hello/widget/ui/view/TestPathImageView.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 
 import androidx.annotation.NonNull;
@@ -33,7 +34,37 @@ public class TestPathImageView extends AppCompatImageView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        drawTriangleBitmap(canvas);
+        drawOvalBitmap(canvas);
+    }
+
+    /**
+     * 画椭圆
+     *
+     * @param canvas
+     */
+    private void drawOvalBitmap(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(5);
+        paint.setStyle(Paint.Style.FILL);
+
+        Path path = new Path();
+
+        int width = getWidth();
+        int height = getHeight();
+
+        //RectF rectF = new RectF(0,0,width,height); 椭圆的外切矩形区域，如果外部是正方形，这个椭圆就是圆了
+        RectF rectF = new RectF(0, 0, width, height * 0.8F);
+        path.addOval(rectF, Path.Direction.CW);
+
+        // 让画的区域变成外部的，比如内部画了一个椭圆，设置这个之后，变成中间扣了一个椭圆
+        //path.setFillType(Path.FillType.INVERSE_EVEN_ODD);
+
+        // 大圆的一半高度-椭圆的上下高度，等于要平移的距离
+        float offsetY = height / 2 - rectF.centerY();
+        path.offset(0, offsetY);
+
+        canvas.drawPath(path, paint);
     }
 
     /**
@@ -45,21 +76,21 @@ public class TestPathImageView extends AppCompatImageView {
         Paint paint = new Paint();
         paint.setColor(Color.RED);
         paint.setStrokeWidth(5);
-        paint.setStyle(Paint.Style.STROKE);
+        paint.setStyle(Paint.Style.FILL);
 
         int width = getWidth();
         int height = getHeight();
 
         Path path = new Path();
 
-        path.moveTo(width/2,paint.getStrokeWidth());
+        path.moveTo(width / 2, paint.getStrokeWidth());
 
-        path.lineTo(0,height-paint.getStrokeWidth());
+        path.lineTo(0, height - paint.getStrokeWidth());
 
-        path.lineTo(width-paint.getStrokeWidth(),height-paint.getStrokeWidth());
+        path.lineTo(width - paint.getStrokeWidth(), height - paint.getStrokeWidth());
         path.close();
 
-        canvas.drawPath(path,paint);
+        canvas.drawPath(path, paint);
     }
 
     /**
@@ -79,7 +110,7 @@ public class TestPathImageView extends AppCompatImageView {
         // 第一个点的坐标
         path.lineTo(100, 100);
         // 第二个点的坐标
-        path.lineTo(100,200);
+        path.lineTo(100, 200);
 
         canvas.drawPath(path, paint);
     }
