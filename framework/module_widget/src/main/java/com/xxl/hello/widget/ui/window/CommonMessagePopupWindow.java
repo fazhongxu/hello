81c1979d5cd582a8fce884470778b0106d3a9ed0
xxl <xuzf@edujia.com>
2024-04-26 17:28:15 +0800

    异常弹窗调整为常用弹窗

--- framework/module_widget/src/main/java/com/xxl/hello/widget/ui/window/CommonMessagePopupWindow.java
+++ framework/module_widget/src/main/java/com/xxl/hello/widget/ui/window/CommonMessagePopupWindow.java
@@ -13,14 +13,19 @@ import com.xxl.kit.ViewUtils;
 import razerdp.basepopup.BasePopupWindow;
 
 /**
- * 异常信息弹窗
+ * 常用信息弹窗
  *
  * @author xxl.
  * @date 2024/04/25.
  */
-public class ExceptionPopupWindow extends BasePopupWindow {
+public class CommonMessagePopupWindow extends BasePopupWindow {
 
     //region: 成员变量
+
+    /**
+     * 标题
+     */
+    private TextView mTvTitle;
 
     /**
      * 消息
@@ -41,14 +46,14 @@ public class ExceptionPopupWindow extends BasePopupWindow {
 
     //region: 构造函数
 
-    public ExceptionPopupWindow(@NonNull final Activity activity) {
+    public CommonMessagePopupWindow(@NonNull final Activity activity) {
         super(activity);
         setPopupGravity(Gravity.CENTER);
         setupLayout();
     }
 
-    public static ExceptionPopupWindow from(@NonNull final Activity activity) {
-        return new ExceptionPopupWindow(activity);
+    public static CommonMessagePopupWindow from(@NonNull final Activity activity) {
+        return new CommonMessagePopupWindow(activity);
     }
 
     //endregion
@@ -59,7 +64,8 @@ public class ExceptionPopupWindow extends BasePopupWindow {
      * 设置视图
      */
     private void setupLayout() {
-        View rootView = createPopupById(R.layout.widget_window_layout_exception);
+        View rootView = createPopupById(R.layout.widget_window_layout_common_message);
+        mTvTitle = ViewUtils.findView(rootView, R.id.tv_title);
         mTvMessage = ViewUtils.findView(rootView, R.id.tv_message);
         mTvCancel = ViewUtils.findView(rootView, R.id.tv_cancel);
         mTvConfirm = ViewUtils.findView(rootView, R.id.tv_confirm);
@@ -71,16 +77,26 @@ public class ExceptionPopupWindow extends BasePopupWindow {
     //region: 提供方法
 
     /**
+     * 设置标题
+     *
+     * @param title
+     * @return
+     */
+    public CommonMessagePopupWindow setTitle(final CharSequence title) {
+        ViewUtils.setText(mTvTitle, title);
+        return this;
+    }
+
+    /**
      * 设置消息
      *
      * @param message
      * @return
      */
-    public ExceptionPopupWindow setMessage(final String message) {
+    public CommonMessagePopupWindow setMessage(final CharSequence message) {
         ViewUtils.setText(mTvMessage, message);
         return this;
     }
-
 
     /**
      * 设置取消按钮
@@ -89,7 +105,7 @@ public class ExceptionPopupWindow extends BasePopupWindow {
      * @param listener
      * @return
      */
-    public ExceptionPopupWindow setNegativeButton(CharSequence text, View.OnClickListener listener) {
+    public CommonMessagePopupWindow setNegativeButton(CharSequence text, View.OnClickListener listener) {
         ViewUtils.setText(mTvCancel, text);
         ViewUtils.setOnClickListener(mTvCancel, v -> {
             listener.onClick(v);
@@ -105,7 +121,7 @@ public class ExceptionPopupWindow extends BasePopupWindow {
      * @param listener
      * @return
      */
-    public ExceptionPopupWindow setPositiveButton(CharSequence text, View.OnClickListener listener) {
+    public CommonMessagePopupWindow setPositiveButton(CharSequence text, View.OnClickListener listener) {
         ViewUtils.setText(mTvConfirm, text);
         ViewUtils.setOnClickListener(mTvConfirm, v -> {
             listener.onClick(v);
