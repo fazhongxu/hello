45e5566779e6ce4ce859c885a35f1fa04505c867
xxl <xuzf@edujia.com>
2024-05-20 16:37:33 +0800

    手机号和身份证号脱敏工具类方法转移到StringUtils,原来的工具类单词比较陌生

--- framework/module_kit/src/main/java/com/xxl/kit/StringUtils.java
+++ framework/module_kit/src/main/java/com/xxl/kit/StringUtils.java
@@ -17,6 +17,8 @@ import androidx.core.content.ContextCompat;
 
 import java.util.IllegalFormatException;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author xxl.
@@ -367,5 +369,76 @@ public final class StringUtils {
             return String.format(Locale.getDefault(), "%s?t=%s", url, TimeUtils.currentServiceTimeMillis());
         }
         return url;
+    }
+
+    /**
+     * 手机号码前三后四脱敏
+     *
+     * @param mobile
+     * @return
+     */
+    public static String mobileEncrypt(String mobile) {
+        try {
+            if (StringUtils.isEmpty(mobile) || (mobile.length() != 11)) {
+                return mobile;
+            }
+            return mobile.replaceAll("(\\d{3})\\d{4}(\\d{4})", "$1****$2");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mobile;
+    }
+
+    /**
+     * 身份证号的保留前三后四，中间的数为星号 "*"
+     *
+     * @param identity
+     * @return
+     */
+    public static String identityEncrypt(String identity) {
+        try {
+            if (StringUtils.isEmpty(identity) || (identity.length() != 18)) {
+                return identity;
+            }
+            return identity.replaceAll("(\\d{3})\\d{9,12}(\\d{4})", "$1****$2");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return identity;
+    }
+
+    /**
+     * 获取手机号和身份证号脱敏
+     *
+     * @param targetText
+     * @return
+     */
+    public static String findPhoneAndIdCardEncrypt(@NonNull final String targetText) {
+        String text = targetText;
+        try {
+            final String REG_PHONE = "(?<!\\d)(?:(?:\\+86|86)?1\\d{2})\\d{4}(\\d{4})(?!\\d)";
+            final String ID_CARD = "(?<!\\d)\\d{6}(\\d{8})\\d{4}(?!\\d)";
+            Pattern phonePattern = Pattern.compile(REG_PHONE);
+            Matcher phoneMatcher = phonePattern.matcher(text);
+            while (phoneMatcher.find()) {
+                String phone = phoneMatcher.group();
+                String replacement = phone.replaceAll("(\\d{3})\\d{4}(\\d{4})", "$1****$2");
+                text = text.replace(phone, replacement);
+            }
+
+            // 匹配身份证号
+            Pattern idCardPattern = Pattern.compile(ID_CARD);
+            Matcher idCardMatcher = idCardPattern.matcher(text);
+            while (idCardMatcher.find()) {
+                String idCard = idCardMatcher.group();
+                String replacement = idCard.replaceAll("(\\d{3})\\d{9,12}(\\d{4})", "$1********$2");
+                text = text.replace(idCard, replacement);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return targetText;
+        }
+
+        return text;
     }
 }
