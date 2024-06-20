6a28e00d1cd368a6bbe10b3c1e72ef6aaa958c84
xxl <xuzf@edujia.com>
2024-06-20 17:31:20 +0800

    代码优化，去除无用代码

--- framework/module_kit/src/main/java/com/xxl/kit/FFmpegUtils.java
+++ framework/module_kit/src/main/java/com/xxl/kit/FFmpegUtils.java
@@ -15,7 +15,6 @@ import com.arthenica.ffmpegkit.MediaInformationSessionCompleteCallback;
 import com.arthenica.ffmpegkit.ReturnCode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
@@ -479,214 +478,6 @@ public class FFmpegUtils {
     }
 
     /**
-     * 替换视频背景颜色
-     *
-     * @param inputVideoPath
-     * @param outputVideoPath
-     * @param targetColor
-     * @param replaceColor
-     */
-    public static FFmpegSession replaceVideoBackgroundColor(@NonNull final String inputVideoPath,
-                                                            @NonNull final String outputVideoPath,
-                                                            @NonNull final String targetColor,
-                                                            @NonNull final String replaceColor) {
-        if (Thread.currentThread() == Looper.getMainLooper().getThread()) {
-            return null;
-        }
-        FileUtils.deleteFile(outputVideoPath);
-//        List<String> command = Arrays.asList(
-//                "-hide_banner",
-//                "-i", inputVideoPath,
-//                "-vf", "chromakey=0x000000:0.1",
-//                "-c:a", "copy",
-//                outputVideoPath
-//        );
-
-//        List<String> command = Arrays.asList(
-//                "-hide_banner",
-//                "-i", inputVideoPath,
-//                "-vf", "chromakey=0x000000:0.1:0.1 [keyed]; [keyed] colorchannelmixer=rr=0:gg=1:bb=0",
-//                "-c:v", "qtrle",
-//                "-c:a", "copy",
-//                outputVideoPath
-//        );
-
-        // ffmpeg -i input.mp4 -vf "chromakey=#3fff08:0.1:0.04" -c:v qtrle -c:a copy output.mov
-
-
-//        ffmpeg -i input.mp4 -vf "chromakey=#3fff08:0.1:0.04" -c:v qtrle -c:a copy output.mov
-
-//        String common = "-hide_banner -i " + inputVideoPath + " -vf chromakey=#3fff08:0.1:0.04 -c:v qtrle -c:a copy " + outputVideoPath;
-
-        List<String> command = Arrays.asList(
-                "-hide_banner",
-                "-i", inputVideoPath,
-                "-vf", "chromakey=#3fff08:0.1:0.04",
-                "-c:v", "qtrle",
-                "-c:a", "copy",
-                outputVideoPath
-        );
-
-        return FFmpegKit.execute(argumentsToString(command.toArray(new String[0])));
-
-
-        // 设置完后还是绿色的
-      /*  String[] command = {
-                "-hide_banner",
-                "-i", inputVideoPath,
-                "-vf", "chromakey=0x3fff08:0.1:0.04,format=yuva420p",
-                "-c:v", "libx264",
-                "-pix_fmt", "yuv420p",
-                "-c:a", "copy",
-                "-movflags", "+faststart",
-                outputVideoPath
-        };*/
-
-//        return FFmpegKit.execute(argumentsToString(command));
-    }
-
-    /**
-     * 检测视频是否有黑色边框
-     *
-     * @param videoPath 视频文件路径
-     * @param callBack  检测结果回调
-     */
-    public static FFmpegSession detectBlackBorders(@NonNull final String videoPath,
-                                                   @Nullable final OnRequestCallBack<Boolean> callBack) {
-        if (Thread.currentThread() == Looper.getMainLooper().getThread()) {
-            return null;
-        }
-
-        final String command = "-hide_banner -i " + videoPath + " -vf blackdetect=d=0.5:pic_th=0.90 -an -f null -";
-
-        return FFmpegKit.executeAsync(command, session -> {
-            final String output = session.getOutput();
-            boolean hasBlackBorder = output != null && output.contains("blackdetect");
-
-            if (callBack != null) {
-                callBack.onSuccess(hasBlackBorder);
-            }
-        });
-    }
-
-    /**
-     * 检测视频是否有透明边框
-     *
-     * @param videoPath 视频文件路径
-     * @param callBack  检测结果回调
-     */
-    public static FFmpegSession detectTransparentBorders(@NonNull final String videoPath,
-                                                         @Nullable final OnRequestCallBack<Boolean> callBack) {
-        if (Thread.currentThread() == Looper.getMainLooper().getThread()) {
-            return null;
-        }
-
-        String command = "-hide_banner -i " + videoPath + " -vf alphaextract -f null -";
-
-        return FFmpegKit.executeAsync(command, session -> {
-            final String output = session.getOutput();
-            boolean hasBlackBorder = output != null && output.contains("alphaextract");
-
-            if (callBack != null) {
-                callBack.onSuccess(hasBlackBorder);
-            }
-        });
-    }
-
-    /**
-     * 生成调色板
-     *
-     * @param videoPath   视频的完整路径
-     * @param dstPath     调色板路径
-     * @param interval    提取视频间隔, 从视频中一秒钟提取多少帧; 建议为5,10, 15;
-     * @param scaleWidth  把视频画面缩放到的宽度
-     * @param scaleHeight 缩放到的高度
-     * @param callBack    FFmpeg执行结果的监听器
-     */
-    public static FFmpegSession executeConvertVideoPalettegen(String videoPath, String dstPath, int interval, int scaleWidth, int scaleHeight, OnRequestCallBack<Boolean> callBack) {
-        if (Thread.currentThread() == Looper.getMainLooper().getThread()) {
-            return null;
-        }
-
-        List<String> commands = new ArrayList<>();
-
-//        commands.add("-v");
-//        commands.add("warning");
-//        commands.add("-i");
-//        commands.add(videoPath);
-//
-//        commands.add("-r");
-//        commands.add(String.valueOf(interval));
-//
-//        commands.add("-vf");
-//        commands.add("fps=20,scale=" + scaleWidth + ":" + scaleHeight + ":flags=lanczos,palettegen");
-//        commands.add("-y");
-//        commands.add(dstPath);
-
-        commands.add("-v");
-        commands.add("warning");
-        commands.add("-i");
-        commands.add(videoPath);
-
-        commands.add("-r");
-        commands.add(String.valueOf(interval));
-
-        commands.add("-vf");
-        commands.add("fps=20,scale=" + scaleWidth + ":" + scaleHeight + ":flags=lanczos,palettegen");
-        //commands.add("-c:v");
-        //commands.add("png");  // Explicitly specifying the PNG encoder
-        commands.add("-y");
-        commands.add(dstPath);
-
-        String command = argumentsToString(commands.toArray(new String[0]));
-
-        return executeAsync(command, callBack);
-    }
-
-    /**
-     * 视频转GIF
-     *
-     * @param videoPath      视频的完整路径
-     * @param palettegenPath 调色板路径
-     * @param dstPath        输出GIF的路径
-     * @param interval       提取视频间隔, 从视频中一秒钟提取多少帧; 建议为5,10, 15;
-     * @param scaleWidth     把视频画面缩放到的宽度
-     * @param scaleHeight    缩放到的高度
-     * @param speed          速度. 转换为gif后的,gif播放速度,建议为0.3,0.5,1.0(不变),1.2,1.5,2.0(放慢一倍);
-     * @param callback       FFmpeg执行结果的监听器
-     */
-    public static FFmpegSession executeConvertVideoToGif(String videoPath, String palettegenPath, String dstPath, int interval, int scaleWidth, int scaleHeight, float speed, OnRequestCallBack<Boolean> callback) {
-
-        // 可以但是模糊
-//        String command = String.format("-hide_banner -i %s -vf fps=%d,scale=%d:-1:flags=lanczos -an %s", videoPath, interval, scaleHeight, dstPath);
-//        return executeAsync(command,null);
-
-        String filter = String.format(Locale.getDefault(), "setpts=%f*PTS,scale=%dx%d:flags=lanczos[x],[x][1:v]paletteuse", speed, scaleWidth, scaleHeight);
-        List<String> commands = new ArrayList();
-
-        commands.add("-i");
-        commands.add(videoPath);
-
-        commands.add("-i");
-        commands.add(palettegenPath);
-
-        commands.add("-r");
-        commands.add(String.valueOf(interval));
-
-        commands.add("-lavfi");
-        commands.add(filter);
-
-        commands.add("-b");
-        commands.add("20k");
-
-        commands.add("-y");
-        commands.add(dstPath);
-
-
-        return executeAsync(argumentsToString(commands.toArray(new String[0])), null);
-    }
-
-    /**
      * 视频抽帧
      *
      * @param inputVideoPath
@@ -695,6 +486,9 @@ public class FFmpegUtils {
      * @return
      */
     public static FFmpegSession videoFrameExtraction(String inputVideoPath, String maskImagePath, String frameDir) {
+        if (Thread.currentThread() == Looper.getMainLooper().getThread()) {
+            return null;
+        }
         FileUtils.createOrExistsDir(frameDir);
         String[] cmd = {
                 "-i", inputVideoPath,
@@ -708,90 +502,28 @@ public class FFmpegUtils {
     /**
      * 将帧合成为GIF图片
      *
-     * @param frameDir   包含帧图像的目录路径
-     * @param outGifPath 输出GIF图片的路径
-     * @param frameRate  GIF的帧率
-     * @param gifWidth   GIF的宽度（可选）
-     * @param gifHeight  GIF的高度（可选）
-     * @return FFmpegSession 对象，可以用于跟踪执行进度和处理结果
+     * @param inputVideoPath
+     * @param maskImagePath  遮罩图片，不透明部分有内容
+     * @param outGifPath
+     * @return
      */
-    public static FFmpegSession frame2Gif(String frameDir, String outGifPath, int frameRate, Integer gifWidth, Integer gifHeight) {
+    public static FFmpegSession video2Gif(String inputVideoPath, String maskImagePath, String outGifPath) {
+        if (Thread.currentThread() == Looper.getMainLooper().getThread()) {
+            return null;
+        }
+        List<String> commands = new ArrayList<>();
+        commands.add("-i");
+        commands.add(inputVideoPath);
+        commands.add("-i");
+        commands.add(maskImagePath);
+        commands.add("-filter_complex");
+        commands.add("[0:v][1:v]alphamerge,format=rgba,crop=in_w:in_h:0:0,fps=10,split[v1][v2];[v1]palettegen[p];[v2][p]paletteuse=new=1");
+        commands.add("-y");
+        commands.add(outGifPath);
 
-//        String[] paletteCmd = {
-//                "-i", frameDir + "/frame_%04d.png",
-//                "-vf", "palettegen",
-//                frameDir + "/palette.png"
-//        };
-//
-//        FFmpegKit.execute(argumentsToString(paletteCmd));
-//
-//        String[] gifCmd = {
-//                "-framerate", "10",
-//                "-i", frameDir + "/frame_%04d.png",
-//                "-i", frameDir + "/palette.png",
-//                "-filter_complex", "[0:v][1:v]paletteuse",
-//                outGifPath
-//        };
-//
-//        FFmpegKit.execute(argumentsToString(gifCmd));
-
-//        String[] gifCmd = {
-//                "-framerate", "10",
-//                "-i", frameDir + "/frame_%04d.png",
-//                "-vf", "fps=10,scale=320:-1:flags=lanczos,palettegen=stats_mode=full[p];[0:v][p]paletteuse=new=1",
-//                outGifPath
-//        };
-
-        // 分开可以实现
-        String[] gifCmd = {
-                "-framerate", "10",
-                "-i", frameDir + "/frame_%04d.png",
-                "-filter_complex", "[0:v]fps=10,scale=320:-1:flags=lanczos,palettegen=stats_mode=full[p];[0:v][p]paletteuse=new=1",
-                "-y", // 这个选项是覆盖输出文件，如果不需要可以去掉
-                outGifPath
-        };
-
-        FFmpegKit.execute(argumentsToString(gifCmd));
-
-        return null;
+        String[] cmd = commands.toArray(new String[0]);
+        return FFmpegKit.execute(argumentsToString(cmd));
     }
-
-    /**
-     * 将帧合成为GIF图片
-     *
-     * @param frameDir   包含帧图像的目录路径
-     * @param outGifPath 输出GIF图片的路径
-     * @param frameRate  GIF的帧率
-     * @param gifWidth   GIF的宽度（可选）
-     * @param gifHeight  GIF的高度（可选）
-     * @return FFmpegSession 对象，可以用于跟踪执行进度和处理结果
-     */
-    public static FFmpegSession frame2Gif2(String inputVideoPath,String maskImagePath, String outGifPath) {
-
-        String[] cmd = {
-                "-i", inputVideoPath,  // 输入视频路径
-                "-i", maskImagePath,   // 输入掩码图片路径
-                "-filter_complex", "[0:v][1:v]alphamerge,format=rgba,crop=in_w:in_h:0:0,fps=10,split[v1][v2];[v1]palettegen[p];[v2][p]paletteuse=new=1",  // 复杂滤镜
-                "-y",  // 覆盖输出文件
-                outGifPath  // 输出GIF路径
-        };
-
-        FFmpegKit.execute(argumentsToString(cmd));
-
-        return null;
-    }
-
-
-
-    // 视频转GIF，一句代码就可以，这个可行
-//    public static FFmpegSession video2Gif(String inputVideoPath, String maskImagePath, String outputGifPath) {
-//        FileUtils.deleteFile(outputGifPath);
-//        String[] cmd = {
-//                "-i", inputVideoPath,
-//                outputGifPath
-//        };
-//        return FFmpegKit.execute(argumentsToString(cmd));
-//    }
 
     public static String argumentsToString(final String[] arguments) {
         if (arguments == null) {
