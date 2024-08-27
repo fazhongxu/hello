59fe94e78cfff65916e44f7629cd27e6fd09126e
xxl <xuzf@edujia.com>
2024-08-27 15:58:36 +0800

    接入walle多渠道打包临时提交，Andres和walle配合使用报错 /Users/xxl/AndroidStudioProjects/hello/app/build/outputs/apk/release/app-release.apk has no v2 signature in Apk Signing Block! 待解决

--- release.sh
+++ release.sh
@@ -5,6 +5,8 @@
 #：       方式二：直接点击第一行左边显示的绿色运行图标即可打包
 #如果提示permission denied，使用命令 chmod u+x release.sh先获取release.sh文件访问权限，再执行./release.sh 命令打包
 #-----------------------------------------------------------------------------------------------------------
-./gradlew resguardRelease
+#./gradlew resguardRelease
 #./gradlew resguardHuaweiRelease(as右边task->andresgrard下找到的) 如果只是需要华为渠道，可以把这个放开，把上面的注释掉，否则就会打多渠道的包
 #./gradlew app:resguardRelease 如果一个项目有多个app，加上app的项目名称即可
+
+./gradlew assembleReleaseChannels
