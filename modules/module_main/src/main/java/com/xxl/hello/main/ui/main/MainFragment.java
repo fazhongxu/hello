a3d2295d9453296530c191d780c33cc0502fdf85
xxl <xuzf@edujia.com>
2024-08-29 17:04:54 +0800

    文本颜色selector设置

--- modules/module_main/src/main/java/com/xxl/hello/main/ui/main/MainFragment.java
+++ modules/module_main/src/main/java/com/xxl/hello/main/ui/main/MainFragment.java
@@ -235,6 +235,7 @@ public class MainFragment extends BaseStateViewModelFragment<MainViewModel, Main
     @Override
     public void onTestClick() {
         UserRouterApi.Login.newBuilder().navigation(getActivity());
+        mViewDataBinding.tvTest.setSelected(true);
     }
 
     /**
