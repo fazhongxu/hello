c46f3f414b5fc56e74efc5702625bbdd52817ae5
xxl <xuzf@edujia.com>
2024-09-14 14:30:58 +0800

    完善代码注释

--- signer.sh
+++ signer.sh
@@ -6,11 +6,13 @@
 #如果提示permission denied，使用命令 chmod u+x signer.sh先获取release.sh文件访问权限，再执行./signer.sh 命令签名
 #-----------------------------------------------------------------------------------------------------------
 
+#/Users/xxl/Library/Android/sdk/build-tools/30.0.3/lib/apksigner.jar
+# apksigner
+
 #build-tools/lib 下 apksigner.jar
 # 安装报错 待解决 https://blog.csdn.net/xys616/article/details/123892541
 
 #Failure [-124: Failed parse during installPackageLI: Targeting R+ (version 30 and above) requires the resources.arsc of installed APKs to be stored uncompressed and aligned on a 4-byte boundary]
 
 java -jar apksigner.jar sign --ks hello --ks-key-alias hello app/build/outputs/apk/release/app-release_100_jiagu.apk
-
 
