34dc16506b230d95a6ce94861e5e1eca3585975d
xxl <xuzf@edujia.com>
2025-01-06 14:45:17 +0800

    图片默认只设置宽高加载后全部显示

--- modules/module_main/src/main/java/com/xxl/hello/main/ui/main/MainFragment.java
+++ modules/module_main/src/main/java/com/xxl/hello/main/ui/main/MainFragment.java
@@ -16,6 +16,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.tbruyelle.rxpermissions3.RxPermissions;
 import com.xxl.core.aop.annotation.Safe;
+import com.xxl.core.image.loader.ImageLoader;
 import com.xxl.core.media.audio.AudioCapture;
 import com.xxl.core.media.audio.AudioCapture.OnAudioFrameCapturedListener;
 import com.xxl.core.media.audio.AudioRecordFormat;
@@ -211,6 +212,20 @@ public class MainFragment extends BaseStateViewModelFragment<MainViewModel, Main
         mMainViewModel.setObservableUserId(String.valueOf(TimeUtils.currentServiceTimeMillis()));
         setupRecord();
         setupRecyclerView();
+
+        // TODO: 2025/1/6
+
+        ///storage/emulated/0/Android/data/com.xxl.hello/files/Pictures/IMG_CROP_20250106_13571263.webp
+
+        ///storage/emulated/0/Android/data/com.xxl.hello/files/Pictures/IMG_CROP_20250106_13575184.jpeg
+
+        //String path = "storage/emulated/0/Android/data/com.xxl.hello/files/Pictures/IMG_CROP_20250106_13571263.webp";
+        //String path = "storage/emulated/0/Android/data/com.xxl.hello/files/Pictures/IMG_CROP_20250106_13575184.jpeg";
+        String path = "/storage/emulated/0/Android/data/com.xxl.hello/files/Pictures/IMG_CROP_20250106_14235137.jpeg";
+
+        ImageLoader.with(this)
+                .load(path)
+                .into(mViewDataBinding.ivImage);
     }
 
     private void setupRecyclerView() {
