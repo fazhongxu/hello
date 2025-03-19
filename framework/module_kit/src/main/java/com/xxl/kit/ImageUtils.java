54e5a3bb4ba04c4e31bd4e11181d9f502dad3658
xxl <xuzf@edujia.com>
2025-03-19 17:19:37 +0800

    图片工具类支持保存图片路径到相册

--- framework/module_kit/src/main/java/com/xxl/kit/ImageUtils.java
+++ framework/module_kit/src/main/java/com/xxl/kit/ImageUtils.java
@@ -1,5 +1,6 @@
 package com.xxl.kit;
 
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -2191,6 +2192,98 @@ public final class ImageUtils {
     }
 
     /**
+     * @param srcImagePath
+     * @param dirName      The name of directory.
+     * @param format       The format of the image.
+     * @return the file if save success, otherwise return null.
+     */
+    @Nullable
+    public static File save2Album(final String srcImagePath,
+                                  final String dirName,
+                                  final Bitmap.CompressFormat format) {
+        String safeDirName = TextUtils.isEmpty(dirName) ? AppUtils.getApplication().getPackageName() : dirName;
+        String suffix = Bitmap.CompressFormat.JPEG.equals(format) ? "JPG" : format.name();
+        String fileName = System.currentTimeMillis() + "." + suffix;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            File picDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+            File destFile = new File(picDir, safeDirName + "/" + fileName);
+            boolean isSuccess = FileUtils.copyFile(srcImagePath, destFile.getAbsolutePath(), null);
+            if (!isSuccess) {
+                return null;
+            }
+            FileUtils.notifySystemToScan(destFile);
+            return destFile;
+        } else {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, fileName);
+            contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/*");
+            Uri contentUri;
+            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            } else {
+                contentUri = MediaStore.Images.Media.INTERNAL_CONTENT_URI;
+            }
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_DCIM + "/" + safeDirName);
+            contentValues.put(MediaStore.MediaColumns.IS_PENDING, 1);
+            ContentResolver resolver = AppUtils.getApplication().getContentResolver();
+            Uri uri = resolver.insert(contentUri, contentValues);
+            if (uri == null) {
+                return null;
+            }
+            FileInputStream is = null;
+            OutputStream os = null;
+            try {
+                try {
+                    byte[] buffer = new byte[1024 * 8];
+                    os = resolver.openOutputStream(uri);
+                    is = new FileInputStream(srcImagePath);
+                    while (true) {
+                        int readSize = is.read(buffer);
+                        if (readSize == -1) {
+                            break;
+                        }
+                        os.write(buffer, 0, readSize);
+                    }
+                    os.flush();
+                    contentValues.put(MediaStore.MediaColumns.IS_PENDING, 0);
+                    resolver.update(uri, contentValues, null, null);
+                    return UriUtils.uri2File(uri);
+                } catch (Exception e) {
+                    resolver.delete(uri, null, null);
+                    e.printStackTrace();
+                    return null;
+                } finally {
+                    try {
+                        if (is != null) {
+                            is.close();
+                        }
+                        if (os != null) {
+                            os.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                resolver.delete(uri, null, null);
+                e.printStackTrace();
+                return null;
+            } finally {
+                try {
+                    if (is != null) {
+                        is.close();
+                    }
+                    if (os != null) {
+                        os.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
      * Return whether it is a image according to the file name.
      *
      * @param file The file.
@@ -2628,7 +2721,7 @@ public final class ImageUtils {
 //                int width = (j == targetCols - 1) ? originWidth - x : pieceWidth;
 //                int height = (i == targetRows - 1) ? originHeight - y : pieceHeight;
                 int width = pieceWidth;
-                int height =  pieceHeight;
+                int height = pieceHeight;
 
                 Bitmap bitmap = Bitmap.createBitmap(originBitmap, x, y, width, height);
                 bitmaps.add(bitmap);
