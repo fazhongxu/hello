c3dfd6325e2235cf5d441c266df4d35d51916bba
xxl <xuzf@edujia.com>
2025-03-28 13:51:07 +0800

    时长转字符串方法添加

--- framework/module_kit/src/main/java/com/xxl/kit/TimeUtils.java
+++ framework/module_kit/src/main/java/com/xxl/kit/TimeUtils.java
@@ -750,15 +750,15 @@ public class TimeUtils {
      * @param timeMs
      * @return
      */
-    public static String stringForTime(long timeMs) {
+    public static final String duration2String(long timeMs) {
         long totalSeconds = timeMs / 1000;
 
-        long seconds = totalSeconds % 60;
-        long minutes = (totalSeconds / 60) % 60;
         long hours = totalSeconds / 3600;
+        long minutes = (totalSeconds / 60) % 60;
+        long seconds = totalSeconds % 60;
 
         if (hours > 0) {
-            return String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, seconds);
+            return String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
         } else {
             return String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
         }
