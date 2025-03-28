7f56febeb450a7e58e67e9b37e6d9cb39367d2c2
xxl <xuzf@edujia.com>
2025-03-28 11:22:40 +0800

    增加格式化时长方法

--- framework/module_kit/src/main/java/com/xxl/kit/TimeUtils.java
+++ framework/module_kit/src/main/java/com/xxl/kit/TimeUtils.java
@@ -745,6 +745,26 @@ public class TimeUtils {
     }
 
     /**
+     * 格式化时长
+     *
+     * @param timeMs
+     * @return
+     */
+    public static String stringForTime(long timeMs) {
+        long totalSeconds = timeMs / 1000;
+
+        long seconds = totalSeconds % 60;
+        long minutes = (totalSeconds / 60) % 60;
+        long hours = totalSeconds / 3600;
+
+        if (hours > 0) {
+            return String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, seconds);
+        } else {
+            return String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        }
+    }
+
+    /**
      * 获取当天零点时刻时间戳
      *
      * @return
