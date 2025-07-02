12835d569ba91e1c3e6897179aa8df22a401a0bf
xxl <xuzf@edujia.com>
2025-07-02 11:27:43 +0800

    消息图片适配

--- framework/module_widget/src/main/java/com/xxl/hello/widget/ui/im/template/ImageMessageTemplateProvider.java
+++ framework/module_widget/src/main/java/com/xxl/hello/widget/ui/im/template/ImageMessageTemplateProvider.java
@@ -1,11 +1,16 @@
 package com.xxl.hello.widget.ui.im.template;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.xxl.core.image.loader.ImageLoader;
 import com.xxl.hello.service.data.model.entity.im.MessageEntity;
 import com.xxl.hello.service.data.model.entity.im.MessageTemplate;
@@ -13,6 +18,7 @@ import com.xxl.hello.service.data.model.entity.im.MessageTemplateType;
 import com.xxl.hello.widget.R;
 import com.xxl.hello.widget.databinding.WidgetRecycleItemMessageImageBinding;
 import com.xxl.kit.ClipboardUtils;
+import com.xxl.kit.DisplayUtils;
 import com.xxl.kit.ToastUtils;
 
 /**
@@ -25,6 +31,21 @@ import com.xxl.kit.ToastUtils;
 public class ImageMessageTemplateProvider extends MessageTemplateProvider {
 
     //region: 成员变量
+
+    /**
+     * 默认宽度
+     */
+    private static final int PREVIEW_DEFAULT_WIDTH = DisplayUtils.dp2px(80);
+
+    /**
+     * 最大宽度
+     */
+    private static final int PREVIEW_MAX_WIDTH = DisplayUtils.dp2px(100);
+
+    /**
+     * 最大高度
+     */
+    private static final int PREVIEW_MAX_HEIGHT = DisplayUtils.dp2px(160);
 
     //endregion
 
@@ -61,9 +82,7 @@ public class ImageMessageTemplateProvider extends MessageTemplateProvider {
                          @Nullable OnMessageTemplateListener listener) {
         WidgetRecycleItemMessageImageBinding imageBinding = DataBindingUtil.bind(rootView);
 
-        ImageLoader.with(rootView.getContext())
-                .load(messageEntity.getMediaPath())
-                .into(imageBinding.ivImage);
+        setupImageLayout(imageBinding, messageEntity);
 
         imageBinding.ivImage.setOnClickListener(v -> {
             if (listener != null && listener.onMessageItemClick(messageEntity)) {
@@ -73,7 +92,7 @@ public class ImageMessageTemplateProvider extends MessageTemplateProvider {
         imageBinding.ivImage.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                if (listener != null && listener.onMessageItemLongClick(imageBinding.llItemContainer,messageEntity)) {
+                if (listener != null && listener.onMessageItemLongClick(imageBinding.llItemContainer, messageEntity)) {
                     return true;
                 }
                 ClipboardUtils.copyText(messageEntity.getMediaPath());
@@ -82,6 +101,59 @@ public class ImageMessageTemplateProvider extends MessageTemplateProvider {
             }
         });
 
+    }
+
+    //endregion
+
+    //region: 页面视图渲染
+
+    /**
+     * 设置图片视图
+     *
+     * @param imageBinding
+     * @param messageEntity
+     */
+    private void setupImageLayout(WidgetRecycleItemMessageImageBinding imageBinding,
+                                  MessageEntity messageEntity) {
+        ImageLoader.with(imageBinding.ivImage)
+                .asBitmap()
+                .load(messageEntity.getMediaPath())
+                .into(new CustomTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        int width = resource.getWidth();
+                        int height = resource.getHeight();
+                        int targetWidth;
+                        int targetHeight;
+                        if (width == 0 || height == 0) {
+                            targetWidth = PREVIEW_DEFAULT_WIDTH;
+                            targetHeight = PREVIEW_DEFAULT_WIDTH;
+                        } else {
+                            if (width > height) {// 按比例缩放
+                                targetWidth = PREVIEW_MAX_WIDTH;
+                                targetHeight = targetWidth * height / width;
+                            } else {
+                                targetHeight = PREVIEW_MAX_HEIGHT;
+                                targetWidth = width * targetHeight / height;
+                            }
+                            if (targetWidth < PREVIEW_DEFAULT_WIDTH) {// 缩放后如果宽度过小，需要调宽，高度裁剪掉
+                                targetWidth = PREVIEW_DEFAULT_WIDTH;
+                                targetHeight = targetWidth * height / width;//按比例缩放后的高
+                                targetHeight = Math.min(targetHeight, PREVIEW_MAX_HEIGHT);
+                            }
+                        }
+                        ViewGroup.LayoutParams layoutParams = imageBinding.ivImage.getLayoutParams();
+                        layoutParams.width = targetWidth;
+                        layoutParams.height = targetHeight;
+                        imageBinding.ivImage.setLayoutParams(layoutParams);
+                        imageBinding.ivImage.setImageBitmap(resource);
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                    }
+                });
     }
 
     //endregion
