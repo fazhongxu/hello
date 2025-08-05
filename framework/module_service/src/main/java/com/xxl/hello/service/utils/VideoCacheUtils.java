8f4b605ebe0dbb25060470754633c7dc7db24dc1
xxl <xuzf@edujia.com>
2025-08-05 10:28:11 +0800

    增加视频缓存工具

---
+++ framework/module_service/src/main/java/com/xxl/hello/service/utils/VideoCacheUtils.java
@@ -0,0 +1,53 @@
+package com.xxl.hello.service.utils;
+
+import android.content.Context;
+import android.text.TextUtils;
+
+import com.danikula.videocache.HttpProxyCacheServer;
+import com.xxl.kit.AppUtils;
+import com.xxl.kit.FileUtils;
+
+import java.io.File;
+
+/**
+ * @author xxl.
+ * @date 2025/8/5.
+ */
+public class VideoCacheUtils {
+
+    private static HttpProxyCacheServer sProxy;
+
+    public static HttpProxyCacheServer getProxy(Context context) {
+        if (sProxy == null) {
+            sProxy = new HttpProxyCacheServer.Builder(context)
+                    .maxCacheSize(512 * 1024 * 1024)
+                    .build();
+        }
+        return sProxy;
+    }
+
+    /**
+     * 获取代理url
+     *
+     * @param url
+     * @return
+     */
+    public static String getProxyUrl(String url) {
+        String proxyUrl = null;
+        try {
+            proxyUrl = getProxy(AppUtils.getApplication())
+                    .getProxyUrl(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return TextUtils.isEmpty(proxyUrl) ? url : proxyUrl;
+    }
+
+    /**
+     * 清除缓存
+     */
+    public static void clearCache() {
+        File cacheDir = getProxy(AppUtils.getApplication()).getCacheRoot();
+        FileUtils.deleteFile(cacheDir);
+    }
+}
