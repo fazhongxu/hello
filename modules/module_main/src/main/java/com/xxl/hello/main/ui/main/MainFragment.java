c559b5c761ac5f720f08ed5a177d772c10ce3a96
xxl <xuzf@edujia.com>
2026-04-17 16:37:44 +0800

    去除分享测试代码

--- modules/module_main/src/main/java/com/xxl/hello/main/ui/main/MainFragment.java
+++ modules/module_main/src/main/java/com/xxl/hello/main/ui/main/MainFragment.java
@@ -47,10 +47,6 @@ import com.xxl.hello.service.handle.api.AppSchemeService;
 import com.xxl.hello.widget.data.router.WidgetRouterApi;
 import com.xxl.hello.widget.ui.view.record.OnRecordListener;
 import com.xxl.hello.widget.ui.view.record.RecordButton;
-import com.xxl.hello.widget.ui.view.share2.OnShareInterceptor;
-import com.xxl.hello.widget.ui.view.share2.ShareBuilder;
-import com.xxl.hello.widget.ui.view.share2.ShareContent;
-import com.xxl.hello.widget.ui.view.share2.SharePlatform;
 import com.xxl.hello.widget.ui.window.MessagePopupWindow;
 import com.xxl.kit.AppUtils;
 import com.xxl.kit.ClipboardUtils;
@@ -59,7 +55,6 @@ import com.xxl.kit.FFmpegUtils;
 import com.xxl.kit.ListUtils;
 import com.xxl.kit.LogUtils;
 import com.xxl.kit.MediaUtils;
-import com.xxl.kit.MomentShareUtils;
 import com.xxl.kit.OnAppStatusChangedListener;
 import com.xxl.kit.OnRequestCallBack;
 import com.xxl.kit.ResourceUtils;
@@ -70,7 +65,6 @@ import com.xxl.kit.ToastUtils;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -244,28 +238,7 @@ public class MainFragment extends BaseStateViewModelFragment<MainViewModel, Main
 
     @Override
     public void onTestClick() {
-//        UserRouterApi.Login.newBuilder().navigation(getActivity());
-
-        ShareContent shareContent = new ShareContent.Builder()
-                .setTitle("标题")
-                .setContent("内容内容")
-                .build();
-        new ShareBuilder(getActivity())
-//                .setPlatforms(Arrays.asList(SharePlatform.WEIXIN, SharePlatform.WEIXIN_CIRCLE))
-                .setContent(shareContent)
-                .share(SharePlatform.WEIXIN);
-//                .setInterceptor(new OnShareInterceptor() {
-//                    @Override
-//                    public boolean onShare(SharePlatform platform, ShareContent content) {
-//                        if (platform == SharePlatform.WEIXIN_CIRCLE) {
-//                            ToastUtils.success("朋友圈").show();
-//                            return true;
-//                        }
-//                        return false;
-//                    }
-//                })
-//                .show();
-
+        UserRouterApi.Login.newBuilder().navigation(getActivity());
     }
 
     /**
